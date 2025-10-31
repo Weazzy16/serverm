30b1e654c0ec80b5f90bf199ce7e297bfbce7e1a
Weazzy16 <91275348+Weazzy16@users.noreply.github.com>
2025-10-31 17:43:45 +0200

    Update index.svelte

--- src_cef/src/popups/circle/index.svelte
+++ src_cef/src/popups/circle/index.svelte
@@ -1,168 +1,201 @@
 <script>
-    import  './assets/css/iconscircle.css';
-    import  './assets/css/circle.sass';
-    import { executeClient } from 'api/rage'
-    import keys from 'store/keys'
-    export let popupData;
+    import './assets/css/iconscircle.css';
+    import './assets/css/circle.sass';
+    import { executeClient } from 'api/rage';
+    import keys from 'store/keys';
+    import { onMount, onDestroy } from 'svelte';
 
+    export let popupData = [];
 
-    $: if (popupData && typeof popupData === "string") 
-        popupData = JSON.parse (popupData)
-    
+    let cooldown = null;
+    let currentPage = 0;
+    const itemsPerPage = 7; // Максимум 7 элементов на странице
+
+    // Аудио
+    let popinSound = null;
+    let popclickSound = null;
+
+    $: if (popupData && typeof popupData === "string") {
+        popupData = JSON.parse(popupData);
+        currentPage = 0;
+    }
+
+    // Вычисляем элементы для текущей страницы
+    $: startIndex = currentPage * itemsPerPage;
+    $: endIndex = startIndex + itemsPerPage;
+    $: currentItems = popupData.slice(startIndex, endIndex);
+    $: hasMore = endIndex < popupData.length;
 
     const updateCategory = (json) => {
-        popupData = JSON.parse (json)
-    }
+        popupData = JSON.parse(json);
+        currentPage = 0;
+    };
+    
     window.events.addEvent("cef.circle.updateCategory", updateCategory);
 
-    import { onDestroy } from 'svelte'
+    const playSound = (sound) => {
+        if (sound) {
+            sound.currentTime = 0;
+            sound.play().catch(err => console.error('Error playing sound:', err));
+        }
+    };
 
-    onDestroy(() => {
-        window.events.removeEvent("cef.circle.updateCategory", updateCategory);
-    });
+    // Выбор элемента - ИСПРАВЛЕНО: передаем правильный item из оригинального массива
+    const select = (item) => {
+        if (!item) return;
+        playSound(popclickSound);
+        // Передаем item.func и item.index (которые уже правильные из popupData)
+        executeClient("client.circle.select", item.func, item.index);
+    };
 
+    // Следующая страница
+    const nextPage = () => {
+        if (hasMore) {
+            playSound(popclickSound);
+            currentPage++;
+        }
+    };
 
-    const    
-        prefix = "circle-";
+    // Закрытие
+    const close = () => {
+        playSound(popclickSound);
+        executeClient("client.circle.select", "back");
+    };
 
+    // Обработчики клавиатуры
+    const onKeyUp = (event) => {
+        if (cooldown) {
+            cooldown = false;
+        }
 
-    let drawname = "Назад"
-    const OnHovered = (name, isBack = false) => {
-        drawname = name;
-        executeClient ("client.circle.isBack", isBack);
-    }
+        switch (event.keyCode) {
+            case 27: // ESC
+                event.preventDefault();
+                close();
+                break;
+        }
+    };
 
-    const onCircleClick = (func, index = 0) => {
-        executeClient ("client.circle.select", func, index);
-    }
-    
-
-    const ontest = (index, max) => {
-        switch (max) {
-            case 1:
-                return 1;
-            case 2:
-                switch (index) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 5;
-                }
-                return;
-            case 3:
-                switch (index) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 3;
-                    case 2:
-                        return 5;
-                }
-                return;
-            case 4:
-                switch (index) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 3;
-                    case 2:
-                        return 5;
-                    case 3:
-                        return 7;
-                }
-                return;
-            case 5:
-                switch (index) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 2;
-                    case 2:
-                        return 4;
-                    case 3:
-                        return 6;
-                    case 4:
-                        return 8;
-                }
-                return;
-            case 6:
-                switch (index) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 2;
-                    case 2:
-                        return 4;
-                    case 3:
-                        return 5;
-                    case 4:
-                        return 6;
-                    case 5:
-                        return 8;
-                }
-                return;
+    const onKeyDown = (event) => {
+        if (cooldown) return;
         
-        }
-        return index + 1;
-    }
+        cooldown = true;
 
-    const defaultCircle__closeWidth = 280;
-    const defaultCircle__closeHeight = 280;
+        const numberKeys = [49, 50, 51, 52, 53, 54, 55, 56, 57]; // 1-9
+        const keyIndex = numberKeys.findIndex(key => key === event.keyCode);
 
-    const initCircle = (node) => {
-        node = node.getBoundingClientRect();
-        if (node) {
-            const percentWidth = (node.width * 100 / defaultCircle__closeWidth) / 100;
-            const percentHeight = (node.height * 100 / defaultCircle__closeHeight) / 100;
-            executeClient ("client.circle.initCircle", percentWidth, percentHeight);
-        }
-    }
-
-
-
-    const handleKeyUp = (event) => {
-        const { keyCode } = event;
-
-        for(let i = 0; i < 8; i++) {
-            if (49 + i == keyCode) {
-                onCircleClick (popupData [i].func, popupData [i].index)
-                return;
+        if (keyIndex !== -1) {
+            // Если нажали 8 и есть кнопка "Ещё..."
+            if (keyIndex === 7 && hasMore && currentItems.length === 7) {
+                nextPage();
+            }
+            // Иначе выбираем элемент
+            else if (keyIndex < currentItems.length) {
+                select(currentItems[keyIndex]);
             }
         }
-    }
+    };
 
+    // Lifecycle
+    onMount(() => {
+        popinSound = new Audio('https://cdn.majestic-files.com/public/master/static/sounds/popin.ogg');
+        popclickSound = new Audio('https://cdn.majestic-files.com/public/master/static/sounds/popclick.ogg');
+        
+        popinSound.volume = 0.3;
+        popclickSound.volume = 0.3;
+        
+        popinSound.load();
+        popclickSound.load();
+        
+        playSound(popinSound);
+        
+        window.addEventListener("keyup", onKeyUp);
+        window.addEventListener("keydown", onKeyDown);
+    });
 
-    const handleKeyDown = (event) => {
-        const { keyCode } = event;
-
-        if (keyCode === $keys[31])
-            return onCircleClick ("back");
-    }
-    
-    const handleMouseUp = (event) => {
-        const { which } = event;
-
-        if (which === 3)
-            onCircleClick ("back");
-    }
+    onDestroy(() => {
+        window.removeEventListener("keydown", onKeyDown);
+        window.removeEventListener("keyup", onKeyUp);
+        window.events.removeEvent("cef.circle.updateCategory", updateCategory);
+        
+        if (popinSound) {
+            popinSound.pause();
+            popinSound = null;
+        }
+        if (popclickSound) {
+            popclickSound.pause();
+            popclickSound = null;
+        }
+    });
 </script>
 
-<svelte:window on:keydown={handleKeyDown} on:keyup={handleKeyUp} on:mouseup={handleMouseUp} />
+<svelte:window on:keydown={onKeyDown} on:keyup={onKeyUp} />
 
-<div class="circle">
-    <div class="circle__close"  use:initCircle on:mouseenter={() => OnHovered ('Назад', true)} on:mouseleave={() => OnHovered ('Назад')} on:click={() => onCircleClick ("back")}>
-        <div class="box-column">
-            <div class="circle__image" class:active={drawname !== "Назад"}></div>
-            <div class="circle__text">{drawname}</div>
+<div class="interaction-main full-width full-height" data-v-b6f590ae>
+    {#if currentItems && currentItems.length}
+        <div data-v-b6f590ae>
+            <!-- Close button -->
+            <div class="close-block" on:click={close} data-v-b6f590ae>
+                <img 
+                    src="https://cdn.majestic-files.com/public/master/static/img/main/interaction/close.svg" 
+                    alt="" 
+                    data-v-b6f590ae 
+                />
+            </div>
+
+            <!-- Actions block -->
+            <div class="actions-block row-block actions-{currentItems.length + (hasMore ? 1 : 0)}" data-v-b6f590ae>
+                <!-- Отображаем текущие элементы (максимум 7) -->
+                {#each currentItems as item, localIndex (`${item.func}_${item.index}_page${currentPage}`)}
+                    <div class="action-block-wrapper" data-v-b6f590ae>
+                        <div 
+                            class="action-block row-block {item.func}"
+                            on:click={() => select(item)}
+                            data-v-b6f590ae
+                        >
+                            <!-- Emoji или иконка -->
+                            {#if item.func !== 'nextpage'}
+                                <div class="action-block__text-emoji" data-v-b6f590ae>
+                                    {item.name.match(/[\p{Emoji}]/u)?.[0] || '📦'}
+                                </div>
+                            {:else}
+                                <img 
+                                    class="action-block__picture-nextpage" 
+                                    src="https://cdn.majestic-files.com/public/master/static/img/main/interaction/nextpage.svg" 
+                                    alt="" 
+                                    data-v-b6f590ae
+                                />
+                            {/if}
+
+                            <!-- Action name -->
+                            <div class="action-block__text-actionName" data-v-b6f590ae>
+                                {item.name.replace(/[\p{Emoji}\s]+/u, '').trim()}
+                            </div>
+                        </div>
+                    </div>
+                {/each}
+
+                <!-- Кнопка "Ещё..." (8-я кнопка) -->
+                {#if hasMore}
+                    <div class="action-block-wrapper" data-v-b6f590ae>
+                        <div 
+                            class="action-block row-block nextpage"
+                            on:click={nextPage}
+                            data-v-b6f590ae
+                        >
+                            <img 
+                                class="action-block__picture-nextpage" 
+                                src="https://cdn.majestic-files.com/public/master/static/img/main/interaction/nextpage.svg" 
+                                alt="" 
+                                data-v-b6f590ae
+                            />
+                            <div class="action-block__text-actionName" data-v-b6f590ae>
+                                Ещё...
+                            </div>
+                        </div>
+                    </div>
+                {/if}
+            </div>
         </div>
-    </div>
-    <div class="center">
-        {#each popupData as data, index}
-        <li on:click={() => onCircleClick (data.func, data.index)} on:mouseenter={() => OnHovered (data.name)} on:mouseleave={() => OnHovered ("Назад")} class="contents child{ontest (index, popupData.length)}">
-            <span class="icons-circle {prefix}{data.func}" />
-            <div>{data.name}</div>
-            <div class="contents__index">{index + 1}</div>
-        </li>
-    {/each}
-    </div>
+    {/if}
 </div>
